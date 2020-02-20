488397b2a0496532d55ba7248aca91f7e046feb6
PoiScript <poiscript@gmail.com>
2020-02-20 14:30:55 +0800

    feat(utils): read_till_element_start

--- xmlparser-derive-utils/src/lib.rs
+++ xmlparser-derive-utils/src/lib.rs
@@ -1,10 +1,12 @@
 pub mod read_text;
+pub mod read_till_element_start;
 pub mod read_to_end;
 pub mod xml_error;
 pub mod xml_escape;
 pub mod xml_unescape;
 
 pub use self::read_text::read_text;
+pub use self::read_till_element_start::read_till_element_start;
 pub use self::read_to_end::read_to_end;
 pub use self::xml_error::{XmlError, XmlResult};
 pub use self::xml_escape::xml_escape;
---
+++ xmlparser-derive-utils/src/read_till_element_start.rs
@@ -0,0 +1,82 @@
+use std::iter::Peekable;
+use xmlparser::{Token, Tokenizer};
+
+use crate::{read_to_end, XmlError, XmlResult};
+
+pub fn read_till_element_start(reader: &mut Peekable<Tokenizer<'_>>, tag: &str) -> XmlResult<()> {
+    while let Some(token) = reader.next() {
+        let token = token?;
+        match token {
+            Token::ElementStart { span, .. } => {
+                if tag == &span.as_str()[1..] {
+                    break;
+                } else {
+                    // log::info!(
+                    //     "Unhandled tag: {:?} when reading ParentElement {}. Skipping.",
+                    //     __tag, stringify!(#ele_name)
+                    // );
+                    read_to_end(reader, &span.as_str()[1..])?;
+                }
+            }
+            Token::ElementEnd { .. }
+            | Token::Attribute { .. }
+            | Token::Text { .. }
+            | Token::Cdata { .. } => {
+                return Err(XmlError::UnexpectedToken {
+                    token: format!("{:?}", token),
+                });
+            }
+            _ => (),
+        }
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_read_till_element_start() -> XmlResult<()> {
+    let mut reader = Tokenizer::from("<tag/>").peekable();
+
+    read_till_element_start(&mut reader, "tag")?;
+    assert!(reader.next().is_some()); // "/>"
+    assert!(reader.next().is_none());
+
+    let mut reader = Tokenizer::from("<parent><skip/><tag/></parent>").peekable();
+
+    assert!(reader.next().is_some()); // "<parent"
+    assert!(reader.next().is_some()); // ">"
+    read_till_element_start(&mut reader, "tag")?;
+    assert!(reader.next().is_some()); // "/>"
+    assert!(reader.next().is_some()); // "</parent>"
+    assert!(reader.next().is_none());
+
+    let mut reader = Tokenizer::from("<parent><skip></skip><tag/></parent>").peekable();
+
+    assert!(reader.next().is_some()); // "<parent"
+    assert!(reader.next().is_some()); // ">"
+    read_till_element_start(&mut reader, "tag")?;
+    assert!(reader.next().is_some()); // "/>"
+    assert!(reader.next().is_some()); // "</parent>"
+    assert!(reader.next().is_none());
+
+    let mut reader = Tokenizer::from("<parent><skip><skip/></skip><tag/></parent>").peekable();
+
+    assert!(reader.next().is_some()); // "<parent"
+    assert!(reader.next().is_some()); // ">"
+    read_till_element_start(&mut reader, "tag")?;
+    assert!(reader.next().is_some()); // "/>"
+    assert!(reader.next().is_some()); // "</parent>"
+    assert!(reader.next().is_none());
+
+    let mut reader =
+        Tokenizer::from("<parent><skip><skip></skip></skip><tag/></parent>").peekable();
+
+    assert!(reader.next().is_some()); // "<parent"
+    assert!(reader.next().is_some()); // ">"
+    read_till_element_start(&mut reader, "tag")?;
+    assert!(reader.next().is_some()); // "/>"
+    assert!(reader.next().is_some()); // "</parent>"
+    assert!(reader.next().is_none());
+
+    Ok(())
+}
