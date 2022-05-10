712d85b97e753e3fac861ca0f3ee326507b9bb09
TheSchemm <theschemm@gmail.com>
2022-05-10 08:41:18 -0500

    WIP: added multiple namespace test

--- test-suite/tests/namespaces.rs
+++ test-suite/tests/namespaces.rs
@@ -1,14 +1,16 @@
 use strong_xml::{XmlRead, XmlResult, XmlWrite};
 
-#[derive(XmlWrite, XmlRead, PartialEq, Debug)]
-#[xml(tag = "nested", prefix = "n", ns = "n: http://www.example.com")]
-struct Nested {
-    #[xml(child = "nested")]
-    contents: Vec<Nested>,
-}
-
 #[test]
 fn test() -> XmlResult<()> {
+
+    #[derive(XmlWrite, XmlRead, PartialEq, Debug)]
+    #[xml(tag = "nested", prefix = "n", ns = "n: http://www.example.com")]
+    struct Nested {
+        #[xml(child = "nested")]
+        contents: Vec<Nested>,
+    }
+
+
     let _ = env_logger::builder()
         .is_test(true)
         .format_timestamp(None)
@@ -24,6 +26,45 @@ fn test() -> XmlResult<()> {
         })
         .to_string()?,
         r#"<n:nested xmlns:n="http://www.example.com"><n:nested><n:nested><n:nested/></n:nested></n:nested></n:nested>"#
+    );
+    
+    Ok(())
+}
+
+
+
+#[test]
+fn test2() -> XmlResult<()> {
+
+    #[derive(XmlWrite, XmlRead, PartialEq, Debug)]
+    #[xml(tag = "nested", prefix = "n", ns = "n: http://www.example.com", ns = "n2: http://www.example.com")]
+    struct Nested {
+        #[xml(child="nested")]
+        nested: Vec<Nested>,
+        #[xml(child="nested2")]
+        nested2: Nested2
+    }
+
+    #[derive(XmlWrite, XmlRead, PartialEq, Debug)]
+    #[xml(tag = "nested2", prefix = "n2",  ns = "n2: http://www.example.com")]
+    struct Nested2 {
+        #[xml(attr="nest", prefix = "n2")]
+        value: String
+    }
+
+
+    let _ = env_logger::builder()
+        .is_test(true)
+        .format_timestamp(None)
+        .try_init();
+    
+    assert_eq!(
+        (Nested {
+            nested: vec![],
+            nested2: Nested2 { value: "hello world".into() }
+        })
+        .to_string()?,
+        r#"<n:nested xmlns:n="http://www.example.com" xmlns:n2="http://www.example.com"><n2:nested2 n2:nest="hello world"/></n:nested>"#
     );
     
     Ok(())
